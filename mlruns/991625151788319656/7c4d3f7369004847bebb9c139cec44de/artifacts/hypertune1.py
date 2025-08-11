516c91cd2847911c13dbad011b37a555fe693a75
Rathodpradip1 <pradiprathod45@gmail.com>
2025-08-11 16:58:56 +0530

    hyperparameter tunning done using MLFLOW

---
+++ mlruns/991625151788319656/7c4d3f7369004847bebb9c139cec44de/artifacts/hypertune1.py
@@ -0,0 +1,85 @@
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import mlflow
+
+# load breast cancer dataset
+data = load_breast_cancer()
+x = pd.DataFrame(data.data, columns=data.feature_names)
+y = pd.Series(data.target)
+
+# splitting dataset into training and testing
+x_train, x_test , y_train, y_test = train_test_split(x ,y, test_size=0.2, random_state=42)
+
+#creating RandomForestClassifier model
+rf = RandomForestClassifier()
+
+#Defining parameter grid for GridSearchCV
+param_grid = {
+    'n_estimators' : [10 ,50 , 100],
+    'max_depth' : [None, 10 , 20 , 30],
+}
+
+# applying the GridSearchCV
+grid_search = GridSearchCV(estimator=rf , param_grid=param_grid , cv=5 , n_jobs=-1, verbose=2)
+
+# # run without mlflow here
+# grid_search.fit(x_train , y_train)
+
+# # Displaying the best params and best score
+# best_params = grid_search.best_params_
+# best_score = grid_search.best_score_
+
+# print(best_params)
+# print(best_score)
+
+
+mlflow.set_experiment('breast-cancer-rf-hp')
+
+with mlflow.start_run():
+    # run with mlflow
+    grid_search.fit(x_train , y_train)
+
+    # displaying best params and best score
+    best_params = grid_search.best_params_
+    best_score = grid_search.best_score_
+
+    # log_params
+    mlflow.log_params(best_params)
+
+    # log_metrics
+    mlflow.log_metric("accuracy" , best_score)
+
+    # log training data
+    train_df = x_train.copy()
+    train_df['target'] = y_train
+    train_df = mlflow.data.from_pandas(train_df)
+    mlflow.log_input(train_df , "training")
+
+    # log testing data
+    test_df = x_test.copy()
+    test_df['target'] = y_test
+    test_df = mlflow.data.from_pandas(test_df)
+    mlflow.log_input(test_df , "testing")
+
+    # log source code
+    mlflow.log_artifact(__file__)
+
+    # log best model
+    mlflow.sklearn.log_model(grid_search.best_estimator , "random_forest")
+
+    # set tags
+    mlflow.set_tags("author" , "pradip kumar")
+
+    print(f'best params: {best_params}')
+    print(f'best_score: {best_score}')
+
+
+
+
+
+
+
+
